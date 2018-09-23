7d4da19abc34969e3c0ddec9a284bafaed3d2512
turnspike <hello@turnspike.com>
2018-09-23 16:27:39 +0930

    Add more setup instructions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 # build base docker image
 docker-compose build
 # create new webapp, --force overrides Gemfile.lock
-docker-compose run webapp rails new . --force --database=postgres
+docker-compose run webapp rails new . --force --database=postgresql
+echo edit config/database.yml to include these lines:
+echo   host: db
+echo   username: postgres
+echo   password:
+echo
+echo then run:
+echo docker-compose run webapp rails db:create
